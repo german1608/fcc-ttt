d8ecaa2f263e72b0f3a72516510f8872d513ae6d
German Robayo Paz <germanrobayo33@gmail.com>
2017-09-01 20:04:31 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import css from './sass/index.scss' // eslint-disable-line
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>, document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  // entry: es la que decide que se va a colocar en el dist. en app.js por ejemplo se decidira que se va a usar
+  // entre css blabla
+  entry: './src/app.js',
+  // el output en un solo archivo
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  // module.rules contiene las reglas o mandatos que ejecutara webpack cada vez que lo ejecutemos
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.pug$/,
+        use: ['html-loader', 'pug-html-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    // The following 2 lines enable to check it via IP
+    host: '0.0.0.0',
+    disableHostCheck: true,
+    contentBase: './dist'
+  },
+  devtool: 'inline-source-map',
+  // plugins para html
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.pug'
+    })
+  ]
+}
