ba8fe46503f48cc7dbe8d6ea721aae39fc449ed1
German Robayo Paz <germanrobayo33@gmail.com>
2017-09-03 23:21:48 -0400

    Rebuild App

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import css from '../sass/index.scss' // eslint-disable-line
-import MenuPage from './components/menu/MenuPage'
-
-ReactDOM.render(
-  <MenuPage />, document.getElementById('app')
-)
