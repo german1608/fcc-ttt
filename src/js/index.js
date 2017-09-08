06bab07f8748c4efd82ca11f9260434cd379a0f0
German Robayo Paz <germanrobayo33@gmail.com>
2017-09-07 23:32:19 -0400

    Import .sass

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import {render} from 'react-dom'
 import {HashRouter, Switch, Route} from 'react-router-dom'
 import MainPage from './components/mainPage/MainPage'
 import GamePage from './components/gamePage/GamePage'
+import '../sass/index.sass'
 
 // const Hola = (props) => <h1>Hola</h1>
 
