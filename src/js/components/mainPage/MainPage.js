d33c1b3ee812c9c8386357718d8241c462cd192d
German Robayo Paz <germanrobayo33@gmail.com>
2017-09-07 23:34:29 -0400

    Add some content to render

--- src/js/components/mainPage/MainPage.js
+++ src/js/components/mainPage/MainPage.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
 const MainPage = (props) => {
-  return <h1>Hi from MainPage.js</h1>
+  return (
+    <div className='hero is-fullheight'>
+      <div className="hero-body main-page">
+        <h1 className='has-text-centered'>Tic Tac Toe</h1>
+
+        <div className='menu'>
+          <button>ONE PLAYER</button>
+          <button>TWO PLAYERS</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default MainPage
