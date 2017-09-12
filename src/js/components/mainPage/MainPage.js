83208448afb55cc840342160c86d5926babab668
German Robayo Paz <germanrobayo33@gmail.com>
2017-09-11 23:56:32 -0400

    Add styles and finishing components for <MainPage />

--- src/js/components/mainPage/MainPage.js
+++ src/js/components/mainPage/MainPage.js
@@ -1,18 +1,41 @@
 import React from 'react'
+import Button from './Button'
 
-const MainPage = (props) => {
-  return (
-    <div className='hero is-fullheight'>
-      <div className="hero-body main-page">
-        <h1 className='has-text-centered'>Tic Tac Toe</h1>
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      numPlayers: null
+    }
+  }
+  choosePlayer(i) {
+    return function (e) {
+      this.setState({
+        numPlayers: i
+      })
+    }
+  }
+  render() {
+    const {choosePlayer, state: {numPlayers}} = this
+    return (
+      <div className='hero is-fullheight'>
+        <div className="hero-body main-page">
+          <h1 className='has-text-centered'>Tic Tac Toe</h1>
 
-        <div className='menu'>
-          <button>ONE PLAYER</button>
-          <button>TWO PLAYERS</button>
+          <div className='menu'>
+            <Button text='1P' onClick={choosePlayer(1).bind(this)}/>
+            <Button text='2P' onClick={choosePlayer(2).bind(this)}/>
+          </div>
+          { /*<h2>{numPlayers === null 
+              ? ''
+              : numPlayers === 1
+              ? 'Next'
+              : 'Let\'s play!'
+          }</h2> */}
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default MainPage
