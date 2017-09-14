8f2530ba725c66bfa33ecc3759765bead2f846b7
German Robayo Paz <germanrobayo33@gmail.com>
2017-09-14 00:34:44 -0400

    Add a whitespace to improve readability

--- src/js/components/mainPage/ChooseMenu.js
+++ src/js/components/mainPage/ChooseMenu.js
@@ -4,8 +4,8 @@ import Button from './Button'
 const ChooseMenu = ({onClick, target}) => {
   return (
     <div className='menu'>
-      <Button text='1P' onClick={onClick(1).bind(target)}/>
-      <Button text='2P' onClick={onClick(2).bind(target)}/>
+      <Button text='1P' onClick={onClick(1).bind(target)} />
+      <Button text='2P' onClick={onClick(2).bind(target)} />
     </div>
   )
 }
