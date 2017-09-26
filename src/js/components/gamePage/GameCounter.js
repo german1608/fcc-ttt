662caf494201a2d7c42453196eec5f1bcf3a5449
German Robayo Paz <germanrobayo33@gmail.com>
2017-09-26 00:04:05 -0400

    Add <GameCounter />

---
+++ src/js/components/gamePage/GameCounter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GameCounter = ({name}) => {
+  return (
+    <div className='counter-box' >
+      <h1>{name}</h1>
+      <h1>1</h1>
+    </div>
+  )
+}
+
+export default GameCounter
