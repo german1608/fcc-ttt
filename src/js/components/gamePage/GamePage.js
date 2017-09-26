d7462d0c8f6e1de850aef79aadca4bd219a903fd
German Robayo Paz <germanrobayo33@gmail.com>
2017-09-26 00:03:41 -0400

    Add and finish the markup for <GamePage />

--- src/js/components/gamePage/GamePage.js
+++ src/js/components/gamePage/GamePage.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import GameBoard from './GameBoard'
+import GameCounter from './GameCounter'
 
 class GamePage extends React.Component {
+  constructor(props) {
+    super(props)
+    /* Important chars:
+     * \u25B6 ▶
+     * \u00D7 ×
+     * \u25CB ○
+    */
+  }
   render () {
     return (
-      <div className='game hero is-fullheight'>
-        <div className='hero-body main-page'>
+      <div className='hero is-fullheight'>
+        <div className='hero-body game'>
+          <GameCounter name='P1' counter={1} />
+          <GameBoard />
+          <GameCounter name='P2' counter={1} />
         </div>
       </div>
     )
