8281a26811059576d3037c9a90943e8dea596785
German Robayo Paz <germanrobayo33@gmail.com>
2017-09-29 00:30:23 -0400

    Add the onClick to each <td></td>

--- src/js/components/gamePage/GameBoard.js
+++ src/js/components/gamePage/GameBoard.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const GameBoard = ({board}) => {
+const GameBoard = ({board, onClick}) => {
   const data = {
     o: '੦',
     x: '×',
     " ": ' '
   }
   // Change each value for a <td></td>
-  const boxes = board.map((box, idx) => <td key={idx} >{data[box]}</td> )
+  const boxes = board.map(
+    (box, idx) => <td key={idx} onClick={onClick(idx)}>{data[box]}</td> )
 
   // Group them in 3 arrays of 3 items and wrap each one in <tr></tr>
   const rows = [boxes.slice(0, 3), boxes.slice(3, 6), boxes.slice(6)]
